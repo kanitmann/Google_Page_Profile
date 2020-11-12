f1446f7137544cad4231dae660dfeef45ea510cb
Kanit Mann <kanitmann90@gmail.com>
2020-11-12 23:07:27 +0530

    Script folder added

---
+++ Script/script.js
@@ -0,0 +1,9 @@
+function search() {
+    var term = document.getElementById("search").value;
+    if (term=="Kanit"||term=="kanit"||term=="KANIT"||term=="Kanit Mann"||term=="KANIT MANN"||term=="Kanit mann" || term=="kanit mann"){
+        window.location.href = "Pages/Help.html";
+        }
+    else{
+        window.location.href = "https://www.google.com";
+    }
+  }
