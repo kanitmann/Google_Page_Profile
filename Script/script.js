bdb28868fa7f68aa7e769d6803b6a7b83c345a20
Kanit Mann <kanitmann90@gmail.com>
2020-11-14 14:49:34 +0530

    Script updates

--- Script/script.js
+++ Script/script.js
@@ -1,7 +1,7 @@
 function search() {
     var term = document.getElementById("search").value;
     if (term=="Kanit"||term=="kanit"||term=="KANIT"||term=="Kanit Mann"||term=="KANIT MANN"||term=="Kanit mann" || term=="kanit mann"){
-        window.location.href = "Pages/Help.html";
+        window.location.href = "Kanit.html";
         }
     else{
         window.location.href = "https://www.google.com";
