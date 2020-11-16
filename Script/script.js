783871385e864492a2cfcbf52071f5ede15799c4
Kanit Mann <kanitmann90@gmail.com>
2020-11-16 17:15:29 +0530

    Pop up added

--- Script/script.js
+++ Script/script.js
@@ -7,3 +7,26 @@ function search() {
         window.location.href = "https://www.google.com";
     }
   }
+
+
+var modal = document.getElementById("pop");
+
+// Get the button that opens the modal
+document.getElementsByClassName("clickme").addEventListener("click", function()
+{  document.getElementById("mypop").style.display = "block";
+})
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("end")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onClick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onClick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
