5107bb31739e8258f73abfe51b66651a8a605801
Kanit Mann <kanitmann90@gmail.com>
2020-11-16 18:02:15 +0530

    Pop up added

--- Script/script.js
+++ Script/script.js
@@ -11,22 +11,10 @@ function search() {
 
 var modal = document.getElementById("pop");
 
-// Get the button that opens the modal
-document.getElementsByClassName("clickme").addEventListener("click", function()
-{  document.getElementById("mypop").style.display = "block";
-})
+document.querySelector(".clickme").addEventListener("click", function () {
+	document.getElementById("mypop").style.display = "block";
+});
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("end")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onClick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onClick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+document.querySelector(".ok").addEventListener("click", function () {
+	document.getElementById("mypop").style.display = "none";
+});
