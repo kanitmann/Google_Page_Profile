b8455ecc95451c5ec4010f051706388236061228
Kanit Mann <kanitmann90@gmail.com>
2020-11-16 20:12:09 +0530

    Search button issue fixed

--- Script/script.js
+++ Script/script.js
@@ -11,6 +11,18 @@ document.getElementById("search").addEventListener("keyup", function(event) {
             }
     }
 });
+
+function search(){
+    var term = document.getElementById("search").value;
+    if (term=="Kanit"||term=="kanit"||term=="KANIT"||term=="Kanit Mann"||term=="KANIT MANN"||term=="Kanit mann" || term=="kanit mann"){
+        window.location.href = "Kanit.html";
+        }
+    else{
+        window.location.href = "https://www.google.com";
+    }
+};
+
+
 document.querySelector(".clickme").addEventListener("click", function () {
 	document.getElementById("mypop").style.display = "block";
 });
